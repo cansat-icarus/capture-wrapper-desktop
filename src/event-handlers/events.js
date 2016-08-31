8e25025bfddc36dc1829d1416bd4a5839690ceeb
André Breda <addobandre99@gmail.com>
2016-08-31 14:45:52 +0100

    Implement packet card list
    Automatically shows the last ~20 packets received, animates their entry
    (and exit when they're kicked out). <cg-packet> elements are recycled
    to maintain performance.

--- src/event-handlers/events.js
+++ src/event-handlers/events.js
@@ -2,4 +2,6 @@ const { bridge } = require('../index')
 const station = require('../station')()
 
 bridge.on('stationScore', () => bridge.emit('stationScore', station.classifier.stationScore))
+
+station.on('packet', packet => bridge.emit('packet', packet))
 station.classifier.on('stationScore', score => bridge.emit('stationScore', score))
