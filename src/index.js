1c706db96b3ae30424555e99b2282b48024e3f41
André Breda <addobandre99@gmail.com>
2016-09-18 12:52:33 +0100

    Move name dialog to different window.
    A cleaner way to split things without annoying window refreshing, in preparation to move database and logging duties to the wrapper.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
-const { EventEmitter } = require('events')
 
-// Prevent window being garbage collected
-let window
+let dialogWindow
 
-// Event handling thingie
-const eventBridge = new EventEmitter()
+// Listen to name setting!
+function messageListener(event, name, nameConfirm) {
+  console.log(arguments)
+  if(typeof name === 'string') {
+    if(name === nameConfirm) {
+      // Create station and logs and whatnot
+      require('./station')(name)
 
-// .emit sends events to the outside (ipcRenderer)
-eventBridge._emitInternal = eventBridge.emit
-eventBridge.emit = (event, ...args) => {
-  window.webContents.send('asynchronous-message', event, ...args)
+      // Launch the main window and close the current one!
+      require('./main-window')
+
+      dialogWindow.close()
+      dialogWindow.once('closed', () => dialogWindow = null)
+
+      // Remove event listener
+      ipcMain.removeListener('asynchronous-message', messageListener)
+    } else if(name === 'exit') {
+      app.quit()
+    }
+  }
 }
 
-// .on/.once/... receive events from the outside (ipcRenderer)
-ipcMain.on('asynchronous-message', (eventObj, ...args) => {
-  eventBridge._emitInternal(...args)
-})
-
-// Handle close button
-eventBridge.on('close', () => app.quit())
-
-// Export event bridge and window
-exports.bridge = eventBridge
-exports.getWindow = () => window
+ipcMain.on('asynchronous-message', messageListener)
 
 // Create window when ready
 app.on('ready', () => {
-  window = new BrowserWindow({
-    minWidth: 750,
-    minHeight: 400,
-    width: 750,
-    height: 550,
-    useContentSize: true,
+  dialogWindow = new BrowserWindow({
+    width: 420,
+    height: 420,
     frame: false
   })
 
@@ -40,22 +38,8 @@ app.on('ready', () => {
   if(process.env.NODE_ENV === 'dev') {
     require('devtron').install()
 
-    setTimeout(() => window.loadURL('http://localhost:3000/'), 5000)
+    setTimeout(() => dialogWindow.loadURL('http://localhost:3000/name-dialog.html'), 2000)
   } else {
-    window.loadURL(`file:${__dirname}/../ui/index.html`)
+    dialogWindow.loadURL(`file:${__dirname}/../ui/name-dialog.html`)
   }
-
-  // Dereference the window, let GC do its thing
-  window.on('closed', () => {
-    window = null
-
-    // Main window close = app quit
-    app.quit()
-  })
-
-  // Start event handlers
-  require('./event-handlers')
-
-  // Ensure a clean exit strategy
-  require('./cleanup')
 })
---
+++ src/main-window.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const { EventEmitter } = require('events')
+
+// Prevent window being garbage collected
+const window = new BrowserWindow({
+  minWidth: 750,
+  minHeight: 400,
+  width: 750,
+  height: 550,
+  useContentSize: true,
+  frame: false
+})
+
+// Dereference the window, let GC do its thing
+window.on('closed', () => {
+  window = null
+
+  // Main window close = app quit
+  app.quit()
+})
+
+if(process.env.NODE_ENV === 'dev')
+  window.loadURL('http://localhost:3000/')
+else
+  window.loadURL(`file:${__dirname}/../ui/index.html`)
+
+// Event handling thingie
+const eventBridge = new EventEmitter()
+
+// .emit sends events to the outside (ipcRenderer)
+eventBridge._emitInternal = eventBridge.emit
+eventBridge.emit = (event, ...args) => {
+  window.webContents.send('asynchronous-message', event, ...args)
+}
+
+// .on/.once/... receive events from the outside (ipcRenderer)
+ipcMain.on('asynchronous-message', (eventObj, ...args) => {
+  eventBridge._emitInternal(...args)
+})
+
+// Handle close button
+eventBridge.on('close', () => app.quit())
+
+// Export event bridge and window
+exports.bridge = eventBridge
+exports.window = window
+
+// Start event handlers
+require('./event-handlers')
+
+// Ensure a clean exit strategy
+require('./cleanup')
