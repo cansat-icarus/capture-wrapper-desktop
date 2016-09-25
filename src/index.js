90629115974167800fe6b00606d05d8a051b25dc
André Breda <addobandre99@gmail.com>
2016-09-25 22:00:20 +0100

    Fix dev-only quick start

--- src/index.js
+++ src/index.js
@@ -28,18 +28,25 @@ ipcMain.on('asynchronous-message', messageListener)
 
 // Create window when ready
 app.on('ready', () => {
+  if(process.env.NODE_ENV === 'dev') require('devtron').install()
+
+  // DEVELOPMENT ONLY: circumvent annoying prompt
+  if(process.env.NODE_ENV === 'dev' && !process.env.NO_QUICK_START) {
+    // Set a dev-only name!
+    messageListener('devstation-do-not-use---ever', 'devstation-do-not-use---ever')
+    return
+  }
+
+  // Create the name dialog window
   dialogWindow = new BrowserWindow({
     width: 420,
     height: 420,
     frame: false
   })
 
-  // Wait a bit for browser-sync
-  if(process.env.NODE_ENV === 'dev') {
-    require('devtron').install()
-
+  // Wait a bit for browser-sync when necessary
+  if(process.env.NODE_ENV === 'dev')
     setTimeout(() => dialogWindow.loadURL('http://localhost:3000/name-dialog.html'), 2000)
-  } else {
+  else
     dialogWindow.loadURL(`file:${__dirname}/../ui/name-dialog.html`)
-  }
 })
