ff93cd851fbf216d597170c6a30120940125be1b
André Breda <addobandre99@gmail.com>
2016-09-29 14:33:22 +0100

    Add null check for dialogWindow

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ let dialogWindow
 
 // Listen to name setting!
 function messageListener(event, name, nameConfirm) {
-  console.log(arguments)
   if(typeof name === 'string') {
     if(name === nameConfirm) {
       // Create station and logs and whatnot
@@ -13,8 +12,10 @@ function messageListener(event, name, nameConfirm) {
       // Launch the main window and close the current one!
       require('./main-window')
 
-      dialogWindow.close()
-      dialogWindow.once('closed', () => dialogWindow = null)
+      if(dialogWindow) {
+        dialogWindow.close()
+        dialogWindow.once('closed', () => dialogWindow = null)
+      }
 
       // Remove event listener
       ipcMain.removeListener('asynchronous-message', messageListener)
