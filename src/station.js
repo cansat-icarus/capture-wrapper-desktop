67721c826511c4e03288cd04e814c2d38bb5b65f
André Breda <addobandre99@gmail.com>
2017-01-18 22:28:30 +0000

    Move DB and Log to capture-lib
    Makes more sense, especially when implementing the Backend connection.

--- src/station.js
+++ src/station.js
@@ -1,23 +1,19 @@
 const Station = require('capture-lib')
 // const crashReporter = require('crash-reporter')
-const dbs = require('./db')
-const logging = require('./log')
 
 let station
 
 module.exports = name => {
-  if(station) return station
-  if(!name) return false
-  const saneName = name.replace(/[\W_]+/g, '')
+	if (station) {
+		return station
+	}
+	if (!name) {
+		return false
+	}
+	const saneName = name.replace(/[\W_]+/g, '')
 
-  // TODO: setup crash reporter
+	// TODO: setup crash reporter
 
-  // Setup DBs
-  dbs.setup(saneName)
-
-  // Setup logging
-  logging.setup(saneName, dbs.logDb())
-
-  station = new Station(saneName, dbs.getAll(), logging.getLogger())
-  return station
+	station = new Station(saneName)
+	return station
 }
